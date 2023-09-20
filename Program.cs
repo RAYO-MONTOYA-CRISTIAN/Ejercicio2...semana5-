0f6518cac8c76c0e4f4976c42f1fa50a6503fbe1
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-09-20 17:31:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la base: ");
+            int numbase = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el exponente: ");
+            int exponente = int.Parse(Console.ReadLine());
+
+            int potencia = 0;
+
+            for (int i = 0; i < exponente; i++)
+            {
+                potencia = numbase * (numbase);
+            }
+
+            Console.WriteLine("La potencia es: " + potencia);
+            Console.ReadKey();
+
+
+        }
+    }
+}
